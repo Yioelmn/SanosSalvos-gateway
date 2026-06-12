0ea30dee8bc087d5ca125ee2f1b0bc93b044f7e8
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-12 07:09:47 -0400

    modificaciones en firebaseconfig y en applicationyaml

--- src/main/java/com/api/gateway/config/FirebaseConfig.java
+++ src/main/java/com/api/gateway/config/FirebaseConfig.java
@@ -1,7 +1,9 @@
 package com.api.gateway.config;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -12,23 +14,32 @@ import jakarta.annotation.PostConstruct;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${FIREBASE_CONFIG_PATH:local}")
+    private String configPath;
+
     @PostConstruct
-    public void initialize(){
-        try{
-            // lee el archivo de crendecial de resources
-            InputStream serviceAccount = new ClassPathResource("firebase-keys.json").getInputStream();
+    public void initialize() {
+        try {
+            InputStream serviceAccount;
+
+            if ("local".equals(configPath)) {
+                serviceAccount = new ClassPathResource("firebase-keys.json").getInputStream();
+                System.out.println("FIREBASE: Inicializando en modo LOCAL (Resources)");
+            } else {
+                serviceAccount = new FileInputStream(configPath);
+                System.out.println("FIREBASE: Inicializando en modo PRODUCCIÓN desde: " + configPath);
+            }
 
             FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
-            if(FirebaseApp.getApps().isEmpty()){
+            if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
-                System.out.println("FIREBASE ADMIN INICIALIZADO DESDE API GATEWAY");
+                System.out.println("FIREBASE ADMIN INICIALIZADO CON ÉXITO DESDE API GATEWAY");
             }
-        }catch(IOException e){
-            System.err.println("ERROR AL INICIALIZAR FIREBASE DESDE GATEWAY");
+        } catch (IOException e) {
+            System.err.println("ERROR CRÍTICO AL INICIALIZAR FIREBASE DESDE GATEWAY: " + e.getMessage());
         }
     }
-    
 }
